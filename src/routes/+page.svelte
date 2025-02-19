2a3c49815aa786debfc51ddbafe88d008db23a90
Basile Simon <basile@basilesimon.fr>
2025-02-19 13:48:55 +0100

    Move from div to button. Remove accessibility hints screaming at me.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -68,24 +68,26 @@
 		{:else}
 			<div class="flex flex-wrap gap-2">
 				{#each data.cids as cid, index (index)}
-					<div
+					<button
 						on:click={() => loadAttestations(cid)}
+						on:keydown={(event) => event.key === 'Enter' && loadAttestations(cid)}
 						class="cid-item relative z-0 w-30 h-30 bg-gray-200 border border-dashed border-gray-300
-      transition-transform duration-200 transform {selectedCID === cid
+		transition-transform duration-200 transform {selectedCID === cid
 							? 'scale-140 bg-gray-300 border-solid border-gray-800 z-10'
 							: 'hover:scale-120 hover:bg-gray-300 hover:border-solid hover:border-gray-800 hover:z-10 hover:cursor-pointer'}"
 						title={cid.toString()}
+						aria-pressed={selectedCID === cid}
 					>
 						<img src={`https://files.dev.starlinglab.org/${cid}`} alt="" />
 						{#if cid}
 							<div
 								class="absolute inset-0 flex items-center justify-center
-        text-xs text-gray-700 opacity-0 hover:opacity-100"
+		text-xs text-gray-700 opacity-0 hover:opacity-100"
 							>
 								{shortenCID(cid)}
 							</div>
 						{/if}
-					</div>
+					</button>
 				{/each}
 			</div>
 		{/if}
