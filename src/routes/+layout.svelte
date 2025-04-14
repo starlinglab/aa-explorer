e38c4b146a3464af8b8915025765cf4581a7d6da
Basile Simon <basile@basilesimon.fr>
2025-04-14 14:45:22 +0200

    Stick navbar on top

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,9 @@
 	// With the new store approach, endpoints are loaded automatically
 	// so we don't need to call loadEndpointsFromStorage anymore
 	import { endpoints, selectedCID } from '$lib/index';
-	
+
 	let { children } = $props();
-	
+
 	function resetApp() {
 		// Clear the selectedCID in the store
 		selectedCID.set(null);
@@ -19,7 +19,7 @@
 	}
 </script>
 
-<div class="navbar">
+<div class="navbar sticky top-0 bg-white shadow-md z-50">
 	<div class="logo" on:click={resetApp} on:keydown={(e) => e.key === 'Enter' && resetApp()}>
 		<img src="/logo.png" alt="AA Explorer Logo" class="logo-img" />
 		<span>AA Explorer</span>
