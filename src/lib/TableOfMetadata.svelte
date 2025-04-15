4e483e0f6a298cd81baf93b7d1ecf3760c70ac91
Basile Simon <basile@basilesimon.fr>
2025-04-15 13:31:25 +0200

    Legend

--- src/lib/TableOfMetadata.svelte
+++ src/lib/TableOfMetadata.svelte
@@ -35,7 +35,7 @@
 <table class="divide-y divide-gray-200 w-full">
 	<thead>
 		<tr>
-			{#each ['', '', 'Attestation', 'Signer'] as header, i}
+			{#each ['H / S / T', 'Attestation & Signer', 'Value', 'Signer'] as header, i}
 				<th
 					class="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
 					style="width: {i === 2 ? '70%' : '10%'}">{header}</th
@@ -50,9 +50,9 @@
 			>
 				<td class="px-4 py-2 text-l text-gray-700" style="width: 10%">
 					<div class="flex space-x-2">
-						<VerifyButton copy={'✔️'} kind={'hash'} data={attribute} {selectedCID} />
-						<VerifyButton copy={'🔑'} kind={'signature'} data={attribute} />
-						<VerifyButton copy={'🗓️'} kind={'timestamp'} data={attribute} />
+						<VerifyButton kind={'hash'} data={attribute} {selectedCID} />
+						<VerifyButton kind={'signature'} data={attribute} />
+						<VerifyButton kind={'timestamp'} data={attribute} />
 					</div>
 				</td>
 				<td class="px-4 py-2 text-xs text-gray-700 text-right" style="width: 10%">
